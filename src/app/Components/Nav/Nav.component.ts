aedbb91a7404eb66dd3b38c4733e8aee66561b74
AnatoliSmorin <AnatoliSmorin@outlook.com>
2024-04-09 08:56:58 +0200

    mobile navbar

---
+++ src/app/Components/Nav/Nav.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [
+    CommonModule, NgbNavModule
+  ],
+  templateUrl: './Nav.component.html',
+  styleUrl: './Nav.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavComponent {
+
+ }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NgbdAccordionBasic } from './Components/Accordion-basic/Accordion-basic.component';
+import { NavComponent } from './Components/Nav/Nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgbdAccordionBasic],
+  imports: [CommonModule, RouterOutlet, NgbdAccordionBasic, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ChessClone';
 }
+ 
